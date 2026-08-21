3acc682408e4397e574ebf34b8a3ad73088b0281
Jechol Lee <jechol@devall.org>
2026-08-21 09:29:58 +0000

    🎨 Export relationship DSL formatter locals

--- .formatter.exs
+++ .formatter.exs
@@ -16,5 +16,6 @@ spark_locals_without_parens =
 [
   import_deps: [:ash, :ash_archival, :spark],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: spark_locals_without_parens
+  locals_without_parens: spark_locals_without_parens,
+  export: [locals_without_parens: spark_locals_without_parens]
 ]
